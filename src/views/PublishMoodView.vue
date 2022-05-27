d3713ab95690bd3a9f734ed27dd3f32e577119e1
zhijia <1597696755@qq.com>
2022-05-27 19:25:15 +0800

    发布心情页面完成背景页面切换效果

--- src/views/PublishMoodView.vue
+++ src/views/PublishMoodView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="moodContainer">
-    <div class="moodTitle" >发布您今天的心情</div>
+    <div class="moodTitle" >
+      <van-icon name="arrow-left" @click="handleBack" class="moodTitleArrow" size="20"></van-icon>
+      <div>发布您今天的心情</div>
+    </div>
     <div class="contentBox">
       <van-field class="content"
         v-model="content"
@@ -11,11 +14,10 @@
       />
     </div>
     <div class="categoryBox">
-      <div class="categoryItem" v-for="item in categoryList" :key="item.cid">
+      <div class="category" :class="['categoryItem',{'hasSelectCate':cateIndex === item.cid}]" @click="handleClickCate(item.cid)" v-for="item in categoryList" :key="item.cid">
         <van-icon :name="item.icon" size="18px"/>&nbsp;{{ item.title }}
         <div class="redPoint" v-if="item.hasDot === 1"></div>
       </div>
-      <van-icon class="categoryItem" size="18px" name="add-o" />
     </div>
     <div class="photoBox">
       <van-uploader
@@ -40,12 +42,13 @@
 </template>
 
 <script>
-
+import { getCateInfo } from '@/api/home'
 export default {
   name: 'PublishMoodView',
   data () {
     return {
       content: '',
+      cateIndex: -1,
       imageList: [],
       categoryList: [],
       options: [
@@ -54,16 +57,52 @@ export default {
       ]
     }
   },
-  created () {
-
+  async created () {
+    // 获取分类列表
+    const res = await getCateInfo()
+    this.categoryList = res.list
   },
   methods: {
     afterRead (file) {
       console.log(this.imageList)
     },
-
     handleSubmit () {
       console.log('点击了提交按钮')
+    },
+    handleBack () {
+      this.$router.go(-1)
+    },
+    handleClickCate (cid) {
+      console.log(cid)
+      this.cateIndex = cid
+      var bg = 'background-image: linear-gradient(120deg, #43e57f 0%, #98d7f1 100%)'
+      switch (cid) {
+        case 9:
+          bg = 'background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);'
+          break
+        case 10:
+          bg = 'background-image: linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%);'
+          break
+        case 11:
+          bg = 'background-image: linear-gradient(to top, #ff0844 0%, #ffb199 100%);'
+          break
+        case 12:
+          bg = 'background-image: linear-gradient(to top, #0c3483 0%, #a2b6df 100%, #6b8cce 100%, #a2b6df 100%);'
+          break
+        case 13:
+          bg = 'background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);'
+          break
+        case 14:
+          bg = 'background-image: linear-gradient(to top, #88d3ce 0%, #6e45e2 100%);'
+          break
+        case 15:
+          bg = 'background-image: linear-gradient(to top, #505285 0%, #585e92 12%, #65689f 25%, #7474b0 37%, #7e7ebb 50%, #8389c7 62%, #9795d4 75%, #a2a1dc 87%, #b5aee4 100%);'
+          break
+        case 16:
+          bg = 'background-image: linear-gradient(to right, #434343 0%, black 100%);'
+          break
+      }
+      document.querySelector('.moodContainer').setAttribute('style', bg)
     }
   }
 }
@@ -72,15 +111,22 @@ export default {
 <style scoped>
 .moodContainer{
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   background-image: linear-gradient(120deg, #43e57f 0%, #98d7f1 100%);
   /*background-image: radial-gradient(circle 248px at center, #16d9e3 0%, #30c7ec 47%, #46aef7 100%);*/
   flex-direction: column;
   align-items: center;
+  transition: background-color 2s;
+}
+.bg01{
+  background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%)
 }
 /* 标题 */
 .moodTitle {
+  width: 96%;
+  padding: 10px 0;
+  position: relative;
   color: #FFFFFF;
   font-size: 30px;
   font-weight: bold;
@@ -89,7 +135,12 @@ export default {
   align-items: center;
   margin-top: 30px;
 }
+.moodTitleArrow{
+  position: absolute;
+  left: 0;
+}
 /* mood content区域 */
+
 .contentBox{
   width: 94%;
   padding: 10px;
@@ -103,7 +154,7 @@ export default {
   background-color: rgba(255,255,255,0);
 }
 ::v-deep .van-field__control::-webkit-input-placeholder {
-  color: #4bba9b;
+  color: #ffffff;
   font-size: 0.2rem;
   font-weight: 600;
 }
@@ -111,6 +162,7 @@ export default {
 /* 分类区域 */
 .categoryBox{
   display: flex;
+  justify-content: space-between;
   width: 94%;
   margin: 20px;
   padding: 20px;
@@ -128,11 +180,15 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #1DC9A0;
+  background-color: rgba(29, 201, 160, 0.5);
   padding: 6px 8px;
   margin: 4px;
-  color: #FFFFFF;
+  color: #fdfcfc;
   font-weight: bold;
+  transition: 0.5s;
+}
+.hasSelectCate{
+  background: #16d95e;
 }
 .redPoint{
   position: absolute;
